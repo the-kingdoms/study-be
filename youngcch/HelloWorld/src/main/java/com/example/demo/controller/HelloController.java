2227bcd18af75e7f3dac34b52a43689ad65845b9
youngcch <cyc8649@gmail.com>
2023-07-01 04:43:58 +0900

    add bonus

--- youngcch/HelloWorld/src/main/java/com/example/demo/controller/HelloController.java
+++ youngcch/HelloWorld/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,18 @@ public class HelloController {
 	@RequestMapping("/api/test")
 	public String api() {
 		return "{ \"test\" : \"api test success\" }";
+	}
+	
+	@RequestMapping("/api/testkey")
+	public String bonus(@RequestParam("key") String key) {
+		try {
+            int value = Integer.parseInt(key);
+            JSONObject json = new JSONObject();
+            json.put("key", value);
+            return json.toString();
+        } catch (Exception e1) {
+            return "Error";
+        }
 	}
 }
 
